56191eb455230f168061dc1cd8fadfc3cb8755f6
유지호 <ugho@192.168.nate.com>
2024-11-30 19:25:02 +0900

    Add hook

--- src/contexts/UrlControllProvider.tsx
+++ src/contexts/UrlControllProvider.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import LZString from 'lz-string';
 
 import { Codes } from "types/code";
@@ -19,5 +21,16 @@ export function query2codes(str: string): Codes {
 }
 
 export default function UrlControllProvider(props: { codes: Codes }) {
+    React.useEffect(() => {
+        const handleUrlChange = () => {
+            console.log("URL changed:", window.location.href);
+        };
 
+        window.addEventListener("popstate", handleUrlChange);
+        return () => {
+            window.removeEventListener("popstate", handleUrlChange);
+        };
+    }, []);
+
+    return (<></>)
 }
