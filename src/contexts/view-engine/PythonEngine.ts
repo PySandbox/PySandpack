bb8b3ff009c5dd75c342148afb55f10fa3c30069
유지호 <ugho@192.168.nate.com>
2024-12-01 20:10:07 +0900

    Fix running screen

--- src/contexts/view-engine/PythonEngine.ts
+++ src/contexts/view-engine/PythonEngine.ts
@@ -94,9 +94,12 @@ export default class PythonEngine implements Engine<Pyodide.PackageData> {
         import sys
         from js import console
         from js import document
-        
+
         previewContainer = document.getElementById("${PREVIEW_CONTAINER}")
         runtimeErrorContainer = document.getElementById("${RUNTIME_ERROR_CONTAINER}")
+
+        previewContainer.innerHTML = ''
+        runtimeErrorContainer.innerHTML = ''
 
         class CustomOutput:
             def write(self, text):
@@ -111,7 +114,10 @@ export default class PythonEngine implements Engine<Pyodide.PackageData> {
         sys.stdout = CustomOutput()
         `;
 
+        const FONT_CODE = ``;
+
         await this.runCode(REDIRECT_CODE);
+        await this.runCode(FONT_CODE);
     }
 
     // private async initPyodideInCommon(): Promise<Pyodide.PyodideInterface> {
